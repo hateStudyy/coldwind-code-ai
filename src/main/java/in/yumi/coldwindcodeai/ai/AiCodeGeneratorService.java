4466a34ef8c15bd878489dd679b92b3bf7d58a25
yumi <835094257@qq.com>
2025-09-04 11:29:45 +0800

    [后端] feat(ai): 添加 Vue 项目生成功能
    - 新增 Vue 项目生成相关的系统提示文件
    - 实现 Vue 项目生成的 AI服务接口和工具类
    - 更新代码生成类型枚举，增加VUE_PROJECT 选项
    - 添加推理流式模型配置

--- src/main/java/in/yumi/coldwindcodeai/ai/AiCodeGeneratorService.java
+++ src/main/java/in/yumi/coldwindcodeai/ai/AiCodeGeneratorService.java
@@ -9,6 +9,16 @@ import reactor.core.publisher.Flux;
 
 public interface AiCodeGeneratorService {
 
+    /**
+     * 生成 Vue 项目代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成过程的流式响应
+     */
+    @SystemMessage(fromResource = "prompt/codegen-vue-project-system-prompt.txt")
+    Flux<String> generateVueProjectCodeStream(@MemoryId long appId, @UserMessage String userMessage);
+
+
     HtmlCodeResult generateHtmlCode(@MemoryId int memoryId, @UserMessage String userMessage);
 
     /**
--- src/main/java/in/yumi/coldwindcodeai/core/AiCodeGeneratorFacade.java
+++ src/main/java/in/yumi/coldwindcodeai/core/AiCodeGeneratorFacade.java
@@ -69,7 +69,8 @@ public class AiCodeGeneratorFacade {
             throw new BusinessException(ErrorCode.SYSTEM_ERROR, "生成类型为空");
         }
         // 根据 appId 获取对应的 AI 服务实例
-        AiCodeGeneratorService aiCodeGeneratorService = aiCodeGeneratorServiceFactory.getAiCodeGeneratorService(appId);        return switch (codeGenTypeEnum) {
+        AiCodeGeneratorService aiCodeGeneratorService = aiCodeGeneratorServiceFactory.getAiCodeGeneratorService(appId, codeGenTypeEnum);
+        return switch (codeGenTypeEnum) {
             case HTML -> {
                 Flux<String> codeStream = aiCodeGeneratorService.generateHtmlCodeStream(userMessage);
                 yield processCodeStream(codeStream, CodeGenTypeEnum.HTML, appId);
@@ -77,6 +78,10 @@ public class AiCodeGeneratorFacade {
             case MULTI_FILE -> {
                 Flux<String> codeStream = aiCodeGeneratorService.generateMultiFileCodeStream(userMessage);
                 yield processCodeStream(codeStream, CodeGenTypeEnum.MULTI_FILE, appId);
+            }
+            case VUE_PROJECT -> {
+                Flux<String> codeStream = aiCodeGeneratorService.generateVueProjectCodeStream(appId, userMessage);
+                yield processCodeStream(codeStream, CodeGenTypeEnum.VUE_PROJECT, appId);
             }
             default -> {
                 String errorMessage = "不支持的生成类型：" + codeGenTypeEnum.getValue();
