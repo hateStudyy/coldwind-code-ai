9fa2c046dba681964a64eca132b531b174865cd6
影歌 <115376190+hateStudyy@users.noreply.github.com>
2025-09-23 18:43:49 +0800

    [微服务改造] screenshot 增加内部服务接口 (中间态，无法单独使用)

--- coldwind-code-ai-microservice/coldwind-code-ai-screenshot/src/main/java/in/yumi/coldwindcodeai/ColdwindCodeAiScreenshotApplication.java
+++ coldwind-code-ai-microservice/coldwind-code-ai-screenshot/src/main/java/in/yumi/coldwindcodeai/ColdwindCodeAiScreenshotApplication.java
@@ -1,9 +1,11 @@
 package in.yumi.coldwindcodeai;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class ColdwindCodeAiScreenshotApplication {
 
     public static void main(String[] args) {
---
+++ coldwind-code-ai-microservice/coldwind-code-ai-screenshot/src/main/java/in/yumi/coldwindcodeai/service/impl/InnerScreenshotServiceImpl.java
@@ -0,0 +1,21 @@
+
+package in.yumi.coldwindcodeai.service.impl;
+
+import in.yumi.coldwindcodeai.innerservice.InnerScreenshotService;
+import in.yumi.coldwindcodeai.service.ScreenshotService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboService;
+
+@DubboService
+@Slf4j
+public class InnerScreenshotServiceImpl implements InnerScreenshotService {
+
+    @Resource
+    private ScreenshotService screenshotService;
+
+    @Override
+    public String generateAndUploadScreenshot(String webUrl) {
+        return screenshotService.generateAndUploadScreenshot(webUrl);
+    }
+}
